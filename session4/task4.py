f5e8444970a0299f604fb75af8b749c12a9eae86
Oyaannn <tehapple15@gmail.com>
2024-03-17 16:57:10 +0700

    assignment4

---
+++ session4/task4.py
@@ -0,0 +1,7 @@
+
+initial_price  = float(input("Enter the price = Rp "))
+
+profit         = initial_price * 0.1
+selling_price  = initial_price + profit
+
+print("The price of the goods sold by Siti is = Rp", selling_price)
