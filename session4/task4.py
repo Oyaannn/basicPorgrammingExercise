635fbfa7849b46950bfcc39bce4d63d26e507d4a
Oyaannn <tehapple15@gmail.com>
2024-03-28 21:03:59 +0700

    assignment4

---
+++ session4/task4.py
@@ -0,0 +1,8 @@
+item_name      = str(input("Enter the item name = "))
+initial_price  = float(input("Enter the price = Rp "))
+profit         = initial_price * 0.1
+selling_price  = initial_price + profit
+
+print(" ")
+print("Cosmetic Name = ", item_name)
+print("the price of goods that siti must sell is = Rp", int(selling_price))
